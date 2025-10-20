a501b2d784dbfefb78c3acc69905033d9c85b8f2
developeravik <avikd906@proton.me>
2025-10-20 16:40:34 +0530

    setup redux and configure axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes/index.tsx'
-import { ThemeProvider } from './providers/theme.provider.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import { RouterProvider } from "react-router";
+import { router } from "./routes/index";
+import { ThemeProvider } from "./providers/theme.provider";
+import { Provider as ReduxProvider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <RouterProvider router={router} />
-    </ThemeProvider>
-  </StrictMode>,
-)
+import {store} from "./redux/store";
+
+import { Toaster } from "./components/ui/sonner";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
+    </ReduxProvider>
+  </React.StrictMode>
+);
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,11 +6,11 @@ export default function Register() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="relative hidden bg-muted lg:block">
-        <img
-          src={TravelRegister}
+        {/* <img
+
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8]"
-        />
+        /> */}
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
